0130a5693cf91757f7305872c669ceacf6479ef7
Alex Foster <alex.88.foster@gmail.com>
2023-09-12 13:54:18 +1000

    Add mouse look

--- 03_fps/player.gd
+++ 03_fps/player.gd
@@ -1,20 +1,34 @@
 extends CharacterBody3D
 
 
-const SPEED = 7.0
+const SPEED = 5.0
 const JUMP_SPEED = 10.0
 const GRAVITY = 50.0
+
+var mouse_sensitivity = 0.5
+
+@onready var camera: Camera3D = $Camera3D
+
+
+func _input(event):
+    if event is InputEventMouseMotion:
+        rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+        camera.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+        camera.rotation.x = clamp(camera.rotation.x,deg_to_rad(-90),deg_to_rad(90))
 
 
 func _physics_process(delta):
     var input_direction = get_input_direction()
+    var move_direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.z)).normalized()
     
-    velocity.x = input_direction.x * SPEED
-    velocity.z = input_direction.z * SPEED
-    velocity.y -= GRAVITY * delta
+    velocity.x = move_direction.x * SPEED
+    velocity.z = move_direction.z * SPEED
     
-    if is_on_floor() and Input.is_action_just_pressed("jump"):
-        velocity.y = JUMP_SPEED
+    if is_on_floor():
+        if Input.is_action_just_pressed("jump"):
+            velocity.y = JUMP_SPEED
+    else:
+        velocity.y -= GRAVITY * delta
     
     move_and_slide()
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# 03_fps
+Justin is ghey.
